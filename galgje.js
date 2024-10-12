391928d427a3788f3747928ca3db84d655947b53
JK82NL <jk_dance@hotmail.com>
2024-10-13 00:30:45 +0200

    v0.2 word check + list import

--- galgje.js
+++ galgje.js
@@ -1,11 +1,4 @@
-let pressed = document.querySelectorAll("input");
-console.log(pressed);
-
-// Woord om te testen 
-// function wordGenerator() {
-    
-// }
-
+// use https://github.com/FelixvL/johangalgjeapp/blob/main/galgje.js to impor words.
 function wordGeneration() {
     let gameWord = "secret"
     console.log(gameWord)
@@ -13,44 +6,51 @@ function wordGeneration() {
     console.log(wordLength)
 }
 
-wordGeneration()
-gameDisplay()
+//globals
+wordGeneration();
+gameDisplay();
 
-// global
+
+// global variables
 let usedLetters = [];
 
 // Checken en melden dat of- het woord fout is, of- er meer dan 1 letter is getypt of- er geen letter of iets anders dan een letter is gytypt.
-function gameInput() {
+function gameInput(gameWord, wordLength) {
     input = document.getElementById("inputHangman").value;
-        if (usedLetters.includes(input)) {
-            alert("You have already used this letter.")
-        }
-        else if (input == '') {
-            alert("Type a letter or a word.");
-        }
-        // else if (input == ) {
-        //     alert("Only one letter or a word");
-        // }
-        else if (isLetter(input) == false) {
-            alert("Only letters from the alfabet");
-        }
-        // else if
-        // alert("This is not the correct word")
-        else {
-            wordArray();
-        }
+    if (usedLetters.includes(input)) {          //Check if letter was already used
+        alert("You have already used this letter.");
+    }
+    else if (input == gameWord) {               //Check if word is correctly guessed and restart game
+        alert("Congratulations, you have guessed the word!")
+        restartGame();
+    }
+    else if (input == '') {                     //Check if empty
+        alert("Type a letter or a word.");
+    }
+    else if (input.charAt(0) === input.charAt(1) && input.charAt(1) === input.charAt(2)) {
+        alert("This is not a word.");
+    }
+    else if (isLetter(input) == false) {        //Check if it is a letter using isLetter function
+        alert("Only letters from the alfabet.");
+    } //CHECK THIS ONE. NOT WORKING!!!
+    // else if (input.length == wordLength) { //Check length of chosen gameword with guess.
+    //     alert("Nice try, but that is not the secret word.");
+    // }
+    else if (input.length >= 1) {
+        alert("Only one letter or a word");
+    }
+    else {
+        wordArray();
+    }
     document.getElementById("inputHangman").value = ''; //Maak tekstvak leeg na input.
     gameDisplay()
-    
 }
-
-gameDisplay()
 
 function isLetter(input) {  // Checkt of het wel een letter is.
     return input.toLowerCase() != input.toUpperCase();
-  }
+}
 
-function wordArray() {   
+function wordArray() {
     let addLetters = usedLetters.unshift(input);
     console.log(usedLetters);
     document.getElementById("usedLetterDisplay").innerHTML = usedLetters
@@ -65,21 +65,9 @@ function gameDisplay(wordLength, gameWord) {
 }
 
 function gameMain(gameWord) {
-    
+
 }
 
 function restartGame() {
 
 }
-
-
-let showButton = 0;
-
-pressed.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        showButton = e.target.value;
-        console.log(showButton);
-        document.getElementById("showButtonPress").innerHTML =
-            "Je hebt op knop " + showButton + " gedrukt";
-    });
-});
